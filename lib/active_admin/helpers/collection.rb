4541a22de65507f027c9f09abb95084cf551b4e0
Shawn Gatchell <shawng03@gmail.com>
2016-04-28 10:20:35 -0700

    just fix the circular reference

--- lib/active_admin/helpers/collection.rb
+++ lib/active_admin/helpers/collection.rb
@@ -8,7 +8,7 @@ module ActiveAdmin
       #
       # We remove the ORDER statement to work around this issue.
       def collection_size(c=collection)
-        size = c ? c.reorder("").count : 10
+        size = c.reorder("").count
         # when GROUP BY is used, AR returns Hash instead of Fixnum for .size
         size = size.size if size.kind_of?(Hash)
 
