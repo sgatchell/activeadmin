1ae48d54ec1ad2b40edb824430caf656ad761dc3
Shawn Gatchell <shawng03@gmail.com>
2016-04-27 21:21:23 -0700

    hack to prevent breakage

--- lib/active_admin/helpers/collection.rb
+++ lib/active_admin/helpers/collection.rb
@@ -7,16 +7,16 @@ module ActiveAdmin
       # the ORDER statement mentions a column defined in the SELECT statement.
       #
       # We remove the ORDER statement to work around this issue.
-      def collection_size(collection=collection)
-        size = collection.reorder("").count
+      def collection_size(c=collection)
+        size = c ? c.reorder("").count : 10
         # when GROUP BY is used, AR returns Hash instead of Fixnum for .size
         size = size.size if size.kind_of?(Hash)
 
         size
       end
 
-      def collection_is_empty?(collection=collection)
-        collection_size(collection) == 0
+      def collection_is_empty?(c=collection)
+        collection_size(c) == 0
       end
     end
   end
